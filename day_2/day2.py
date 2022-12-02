94c9ab35664903db95c26d140bbefad21df0870a
Brooke Knowles <brookeknowles7@gmail.com>
2022-12-03 07:25:37 +1300

    part 2 day 2

--- day_2/day2.py
+++ day_2/day2.py
@@ -40,4 +40,64 @@ with open("input.txt") as file:
         
         scores += check_winner(game) + score(game)
 
-print("Total score: ", scores)
+print("Total score (part 1): ", scores)
+
+
+# part 2 below
+
+def determine_new_score(line):
+    if line == "A X":
+        # they chose rock, we need to lose -> 0 pts
+        # our choice must be scissors -> 3 pts
+        return 3
+        
+    elif line == "A Y":
+        # they chose rock, we need to draw -> 3 pts
+        # our choice must be rock -> 1 pts
+        return 4
+
+    elif line == "A Z":
+        # they chose rock, we need to win -> 6 pts
+        # our choice must be paper -> 2 pts
+        return 8
+        
+    elif line == "B X":
+        # they chose paper, we need to lose -> 0 pts
+        # our choice must be rock -> 1 pts
+        return 1
+
+    elif line == "B Y":
+        # they chose paper, we need to draw -> 3 pts
+        # our choice must be paper -> 2 pts
+        return 5
+
+    elif line == "B Z":
+        # they chose paper, we need to win -> 6 pts
+        # our choice must be scissors -> 3 pts
+        return 9
+        
+    elif line == "C X":
+        # they chose scissors, we need to lose -> 0 pts
+        # our choice must be paper -> 2 pts
+        return 2
+
+    elif line == "C Y":
+        # they chose scissors, we need to draw -> 3 pts
+        # our choice must be scissors -> 3 pts
+        return 6
+        
+    elif line == "C Z":
+        # they chose scissors, we need to win -> 6 pts
+        # our choice must be rock -> 1 pts
+        return 7
+        
+
+scores = 0
+with open("input.txt") as file:
+    for line in file:
+        line = line.replace("\n", "")
+        score = determine_new_score(line)
+        scores += score
+
+
+print("Total score (part 2): ", scores)
